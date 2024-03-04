f318b938bc5b2ec1b3a3701a08128d0206ba785f
Saad shahzad <saadshahz369@gmail.com>
2024-03-04 13:41:41 +0500

    team page completed

--- app/team/page.js
+++ app/team/page.js
@@ -4,44 +4,53 @@ import Footer from "../../component/common/Footer";
 import Header from "../../component/common/Header";
 import burger from "../../public/images/burger.png";
 import adidas from "../../public/images/adidas.png";
+import Link from "next/link";
 export default function Team() {
   const team = [
     {
+      key: "1",
       image: "/images/team/1.png",
       name: "John Doe",
       Position: "Manager",
     },
     {
+      key: "2",
       image: "/images/team/1.png",
       name: "Smith joe",
       Position: "Marketing Head",
     },
     {
+      key: "3",
       image: "/images/team/1.png",
       name: "Sager jae",
       Position: "Manager",
     },
     {
+      key: "4",
       image: "/images/team/1.png",
       name: "James Hames",
       Position: "Technical Manager",
     },
     {
+      key: "5",
       image: "/images/team/1.png",
       name: "Will Smith",
       Position: "Developer",
     },
     {
+      key: "6",
       image: "/images/team/1.png",
       name: "Tom Smith",
       Position: "IT Manager",
     },
     {
+      key: "7",
       image: "/images/team/1.png",
       name: "John Doe",
       Position: "Social Meadia Manager",
     },
     {
+      key: "8",
       image: "/images/team/1.png",
       name: "Smith joe",
       Position: "Marketing Agent",
@@ -136,7 +145,11 @@ export default function Team() {
                   <div className="col-md-3">
                     <div className="team_card">
                       <div className="team_card_img">
-                        <Image src={item.image} width={200} height={200} />
+                        <Image
+                          src={`/images/team/${item.key}.jpg`}
+                          width={200}
+                          height={200}
+                        />
                       </div>
                       <div className="team_card_desc">
                         <h4>{item.name}</h4>
@@ -159,15 +172,18 @@ export default function Team() {
             {awards &&
               awards.map((item) => {
                 return (
-                  <div className="col-md-6" key={item.key}>
+                  <div className="col-md-4" key={item.key}>
                     <div className="award-details">
                       <h4>{item.Year}</h4>
-                      <Image
-                        src={`/images/awards/${item.key}.png`}
-                        width={100}
-                        height={100}
-                        alt="Awards"
-                      />
+                      <div>
+                        <Image
+                          src={`/images/awards/${item.key}.png`}
+                          width={100}
+                          height={100}
+                          alt="Awards"
+                        />
+                      </div>
+
                       <h5>{item.title}</h5>
                       <ul>
                         {item.desc.map((desc) => {
@@ -178,6 +194,25 @@ export default function Team() {
                   </div>
                 );
               })}
+          </div>
+        </div>
+      </section>
+
+      <section className="team-btn">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <div style={{ width: "50%" }}>
+                <div className="work-section-more">
+                  <Link href={"/"}>Back To Home</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="work-section-more">
+                <Link href={"#"}>Checkout Our Work</Link>
+              </div>
+            </div>
           </div>
         </div>
       </section>
